b17e56a544c299aef4d71484f936f87004e94125
KLEZ_V3\GFeutren <gfeutren@gmail.com>
2017-06-29 15:39:15 +0200

    A Propos - Mise à jour des vignettes

--- js/nsv-tv-about.js
+++ js/nsv-tv-about.js
@@ -11,11 +11,12 @@ $(document).ready(function () {
                 var presentation = $("#presentation-template").clone();
                 presentation.attr("id", "presentation" + streamer.twitch.channelName);
                 if (null !== channelJSON.logo) {
-                    presentation.find(".presentationImg").attr("src", channelJSON.logo);
+                    presentation.find(".streamerAboutImg").attr("src", channelJSON.logo);
                 } else {
-                    presentation.find(".presentationImg").attr("src", config.twitch.nsvDefaultProfilImg);
+                    presentation.find(".streamerAboutImg").attr("src", config.twitch.nsvDefaultProfilImg);
                 }
-                presentation.find(".presentationStreamerName").html(streamer.twitch.channelName);
+                presentation.find(".streamerAboutHeaderImg").css("background-image", "url(" + channelJSON.profile_banner + ")");
+                presentation.find(".streamerAboutChannelName").html(streamer.twitch.channelName);
                 presentation.find(".presentationText").html(streamer.presentation);
                 $("#presentationDiv").append(presentation);
             });
